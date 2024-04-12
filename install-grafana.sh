a0283b3eadf254f19ddb5063dad3c7ad8681810c
Vighnesh Manthapurwar <95901358+Vighnesh-99@users.noreply.github.com>
2024-04-12 18:13:46 +0530

    Create install-grafana.sh

---
+++ install-grafana.sh
@@ -0,0 +1,7 @@
+wget -q -O - https://packages.grafana.com/gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/grafana.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/grafana.gpg] https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+sudo apt update
+sudo apt install grafana -y
+sudo systemctl start grafana-server
+sudo systemctl status grafana-server
+sudo systemctl enable grafana-server
